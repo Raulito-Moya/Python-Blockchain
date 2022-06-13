56b739f6c60802a61a3b74fd2daf087af9d74802
Raulito-Moya <rauly7moya@gmail.com>
2022-06-13 09:14:18 -0500

    verifing transaction

--- blockchain.py
+++ blockchain.py
@@ -1,20 +1,49 @@
 #Initializing out blockchain list
+from pickle import TRUE
+
+MINING_REWARD = 10
+
 genesis_block = {
-    'previous_hash': 'XYZ', 
+    'previous_hash': '', 
     'index': 0, 
     'transactions':[]
 }
 blockchain = [genesis_block] # -1 for get the last value in thelist
 open_transactions = []
 owner = 'Max'
+participants = {'Max'}
+
+def hash_block(block):
+    print('hash_block:',block)
+    return '-'.join([str(block[key]) for key in block])
 
 
+def get_balance(participant):
+    tx_sender =[[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]  # O|
+    open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
+    tx_sender.append(open_tx_sender)
+    amount_sent = 0
+    for tx in tx_sender:
+          if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+          if len(tx) > 0:
+           amount_received += tx[0]
+    return amount_received - amount_sent
 
 def get_last_blockchain_value():
       """Returns the ast value of the current blockchain"""
       if len(blockchain) < 1:
            return None
       return blockchain[-1]
+
+def verify_transaction(transaction):
+    sender_balance = get_balance(transaction['sender'])
+    return sender_balance >= transaction['amount']
+   
+
 
 def add_transaction(recipient,sender=owner, amount=1.0):
   """Append a new value as well as the last blockchain value to the blockchain
@@ -26,26 +55,39 @@ def add_transaction(recipient,sender=owner, amount=1.0):
 
   
   """
-  transaction = {'sender':sender,'recipient':recipient,'amount':amount}
-  open_transactions.append(transaction)
- 
-  #print(blockchain)
+  transaction = {
+      'sender':sender,
+      'recipient':recipient,
+      'amount':amount
+      }
+  if verify_transaction(transaction):
+      print('Transaction verified!!!')
+      open_transactions.append(transaction)
+      participants.add(sender)
+      participants.add(recipient)
+      return True
+  return False
+      #print(blockchain)
 
 
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
-    print(hashed_block)
-    for key in last_block:
-        value = last_block[key]
-        hashed_block = hashed_block + str(value)
-  
+    hashed_block = hash_block(last_block)
+    reward_transaction = {
+        'sender':'MINING',
+        'recipient':owner,
+        'amount':MINING_REWARD
+    }
+    copied_transaction = open_transactions
+    open_transactions.append(reward_transaction)
+
     block = {
     'previous_hash': hashed_block, 
     'index': len(blockchain), 
     'transactions':open_transactions 
     }
     blockchain.append(block)
+    return True
 
 
 def get_transaction_value():
@@ -67,35 +109,26 @@ def print_blockchain_elements():
           print('-' * 20)    
 
 def verify_chain():
-   # block_index = 0
-    is_valid = True
-    for block_index in range(len(blockchain)):
-         if block_index == 0:
+    """Verify the current blockchain and return True if it's valid """
+   
+    for (index, block) in enumerate(blockchain):
+        if index == 0:
             continue
-         elif blockchain[block_index] [0] == blockchain[block_index - 1]:
-            is_valid = True
-         else:
-             is_valid = False
-           # break
-   # for block in blockchain:
-    #    if block_index == 0:
-    #        block_index += 1
-    #        continue
-    #    elif block[0] == blockchain[block_index - 1]:
-     #       is_valid = True
-     #   else: 
-     #       is_valid = False
-    #        break
-    #    block_index += 1
-    return is_valid
+        if block['previous_hash'] != hash_block(blockchain[index - 1 ]):
+            print('Block!!!!',hash_block(blockchain[index - 1 ]))
+          
+            return False
+    
+    return True
 
-waiting_forinput = True
+waiting_for_input = True
 
 while True:
       print("PLease choose")
       print("1: Add a new transaction value")
       print("2: Mine a new block")
       print('3: Output the blockchain blocks')
+      print('4: Output Participants')
       print('h:Manipulate the chain')
       print('q: Quit')
       user_choice =  get_user_choice()
@@ -103,26 +136,35 @@ while True:
           tx_data = get_transaction_value()
           recipient, amount= tx_data
           #add the transaction amount to the blockchain
-          add_transaction(recipient, amount=amount)
-          print(open_transactions)
+          if add_transaction(recipient, amount=amount):
+           print(open_transactions)
+          else:
+              print('Transaction failed!')
 
       elif user_choice == '2':
-           mine_block()    
+          if mine_block():
+              open_transactions = []    
       elif user_choice == '3':
            print_blockchain_elements()  
-
+      elif user_choice == '4':
+           print(participants)
       elif user_choice == 'h':
           if len(blockchain) >= 1:
-             blockchain[0] = [2]
+             blockchain[0] = {
+                 'previous_hash': '', 
+                 'index': 0, 
+                 'transactions':[{'sender':'Chris', 'recipient':'Max', 'amount':100.0}]}
       elif user_choice == 'q':
           wating_for_input = False
           break
       else:
-          print('Input was invalid, please pick a value from the list')
-     # if not verify_chain():
-     #   print_blockchain_elements()
-      #  print('Invalid blockchain')
-     #   break
+           print('Input was invalid, please pick a value from the list')
+      if not verify_chain():
+         print_blockchain_elements()
+         print('Invalid blockchain')
+         break
+      print(get_balance('Max'))
+    
 else: 
      print('User left!')  
 
