015e5e8e9b020488e5549aa5cd9ace5e14f146ad
Raulito-Moya <rauly7moya@gmail.com>
2022-06-27 16:19:12 -0500

    adding binary data with pickle

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from pickle import TRUE
 from hash_util import hash_string_256, hash_block
 
 import json 
+import pickle
 from collections import OrderedDict
 {
     "field":"value",
@@ -26,10 +27,15 @@ owner = 'Max'
 participants = {'Max'}
 
 def load_data():
-    with open('blockchain.txt',mode='r') as f:
+    with open('blockchain.txt ',mode='r') as f:
+         #file_content = pickle.loads(f.read())
          file_content = f.readlines()
+         
+
          global blockchain
          global open_transactions
+         #blockchain = file_content['chain']
+         #open_transactions = file_content['ot']
          blockchain = json.loads(file_content[0][:-1]) 
          updated_blockchain = []
          for block in blockchain:
@@ -54,9 +60,15 @@ load_data()
 
 def save_data():
     with open('blockchain.txt',mode='w') as f:
-        f.write(json.dumps(blockchain))
-        f.write('\n')
-        f.write(json.dumps(open_transactions))
+      f.write(json.dumps(blockchain))
+      f.write('\n')
+      f.write(json.dumps(open_transactions))
+     #save_data = {
+     #    'chain':blockchain,
+     #    'ot': open_transactions
+     #}
+     #f.write(pickle.dumps(save_data))
+      #f.write('\n')
 
 def hash_block(block):
     print('hash_block:',block)
